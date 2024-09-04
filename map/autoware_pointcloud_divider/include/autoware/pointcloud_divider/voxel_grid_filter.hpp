75642376be49489024c5f5c3ae83ea46cd8270b8
Anh Nguyen <anh.nguyen.2@tier4.jp>
2024-09-04 16:41:16 +0700

    feat(map): add pointcloud divider and pointcloud merger (#100)
    * Added point cloud merger
    
    Signed-off-by: Anh Nguyen <anh.nguyen.2@tier4.jp>
    
    * Update README and remove unused svg file
    
    Signed-off-by: Anh Nguyen <anh.nguyen.2@tier4.jp>
    
    * style(pre-commit): autofix
    
    * Fix pre-commit
    
    Signed-off-by: Anh Nguyen <anh.nguyen.2@tier4.jp>
    
    * style(pre-commit): autofix
    
    * Fix spell check
    
    Signed-off-by: Anh Nguyen <anh.nguyen.2@tier4.jp>
    
    * style(pre-commit): autofix
    
    * Fix pre-commit ci
    
    Signed-off-by: Anh Nguyen <anh.nguyen.2@tier4.jp>
    
    * Fix pre-commit ci
    
    Signed-off-by: Anh Nguyen <anh.nguyen.2@tier4.jp>
    
    * Fix pre-commit ci
    
    Signed-off-by: Anh Nguyen <anh.nguyen.2@tier4.jp>
    
    * Add autoware_ prefix to nodes
    
    Signed-off-by: Anh Nguyen <anh.nguyen.2@tier4.jp>
    
    * style(pre-commit): autofix
    
    * Add autoware_ prefix to pointcloud merger and divider; refactor code; rearrange files
    
    Signed-off-by: Anh Nguyen <anh.nguyen.2@tier4.jp>
    
    * Follow autoware directory structure
    
    Signed-off-by: Anh Nguyen <anh.nguyen.2@tier4.jp>
    
    * Move parameters to yaml files, create schema json files, and apply clang format
    
    Signed-off-by: Anh Nguyen <anh.nguyen.2@tier4.jp>
    
    * Fix an error while reading PCD file
    
    Signed-off-by: Anh Nguyen <anh.nguyen.2@tier4.jp>
    
    * style(pre-commit): autofix
    
    * Fix pre-commit ci
    
    Signed-off-by: Anh Nguyen <anh.nguyen.2@tier4.jp>
    
    * Fix pre-commit-optional
    
    Signed-off-by: Anh Nguyen <anh.nguyen.2@tier4.jp>
    
    * style(pre-commit): autofix
    
    * Fix merger's schema file
    
    Signed-off-by: Anh Nguyen <anh.nguyen.2@tier4.jp>
    
    * style(pre-commit): autofix
    
    * Fixed review comments
    
    Signed-off-by: Anh Nguyen <anh.nguyen.2@tier4.jp>
    
    * style(pre-commit): autofix
    
    * Fixed spell check
    
    Signed-off-by: Anh Nguyen <anh.nguyen.2@tier4.jp>
    
    ---------
    
    Signed-off-by: Anh Nguyen <anh.nguyen.2@tier4.jp>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Yamato Ando <yamato.ando@gmail.com>

---
+++ map/autoware_pointcloud_divider/include/autoware/pointcloud_divider/voxel_grid_filter.hpp
@@ -0,0 +1,53 @@
+// Copyright 2024 Autoware Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef AUTOWARE__POINTCLOUD_DIVIDER__VOXEL_GRID_FILTER_HPP_
+#define AUTOWARE__POINTCLOUD_DIVIDER__VOXEL_GRID_FILTER_HPP_
+
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+
+#include <iostream>
+
+namespace autoware::pointcloud_divider
+{
+
+template <typename PointT>
+class VoxelGridFilter
+{
+  typedef pcl::PointCloud<PointT> PclCloudType;
+  typedef typename PclCloudType::Ptr PclCloudPtr;
+
+public:
+  VoxelGridFilter() { resolution_ = 0; }
+
+  void setResolution(float res)
+  {
+    if (res > 0) {
+      resolution_ = res;
+    }
+  }
+
+  void filter(const PclCloudType & input, PclCloudType & output);
+
+private:
+  float resolution_;
+};
+
+template class VoxelGridFilter<pcl::PointXYZ>;
+template class VoxelGridFilter<pcl::PointXYZI>;
+
+}  // namespace autoware::pointcloud_divider
+
+#endif  // AUTOWARE__POINTCLOUD_DIVIDER__VOXEL_GRID_FILTER_HPP_
---
+++ map/autoware_pointcloud_divider/src/voxel_grid_filter.cpp
@@ -0,0 +1,47 @@
+// Copyright 2024 Autoware Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <autoware/pointcloud_divider/centroid.hpp>
+#include <autoware/pointcloud_divider/grid_info.hpp>
+#include <autoware/pointcloud_divider/voxel_grid_filter.hpp>
+
+#include <unordered_map>
+
+namespace autoware::pointcloud_divider
+{
+
+template <typename PointT>
+void VoxelGridFilter<PointT>::filter(const PclCloudType & input, PclCloudType & output)
+{
+  if (resolution_ <= 0) {
+    return;
+  }
+
+  std::unordered_map<GridInfo<3>, Centroid<PointT>> grid_map;
+
+  for (auto & p : input) {
+    auto gkey = pointToGrid3(p, resolution_, resolution_, resolution_);
+
+    grid_map[gkey].add(p);
+  }
+
+  // Extract centroids
+  output.reserve(grid_map.size());
+
+  for (auto & it : grid_map) {
+    output.push_back(it.second.get());
+  }
+}
+
+}  // namespace autoware::pointcloud_divider
