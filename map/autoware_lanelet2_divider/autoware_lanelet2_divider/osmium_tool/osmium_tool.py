56f3dc57129f72674658661ae3840feaa7cf5f61
Barış Zeren <baris@leodrive.ai>
2024-10-28 15:58:30 +0300

    chore: remove unnecessary else
    Signed-off-by: Barış Zeren <baris@leodrive.ai>

--- map/autoware_lanelet2_divider/autoware_lanelet2_divider/osmium_tool/osmium_tool.py
+++ map/autoware_lanelet2_divider/autoware_lanelet2_divider/osmium_tool/osmium_tool.py
@@ -21,41 +21,41 @@ def extract_osm_file(
             f"Extracted osm file: {input_osm_file_path}", DebugMessageType.SUCCESS
         )
         return True
+    
+    if "Output directory is missing or not accessible" in result.stderr:
+        Debug.log(
+            f"Cannot extracted osm file: {input_osm_file_path}, Output directory is missing or not accessible",
+            DebugMessageType.ERROR,
+        )
+        Debug.log(f"Creating output directory: {output_dir}", DebugMessageType.INFO)
+        os.mkdir(output_dir)
+        extract_osm_file(input_osm_file_path, input_config_file_path, output_dir)
+        return True
+    elif "Way IDs out of order" in result.stderr:
+        Debug.log(
+            f"Cannot extracted osm file: {input_osm_file_path}, Way IDs out of order",
+            DebugMessageType.ERROR,
+        )
+        Debug.log(f"Sorting osm file: {input_osm_file_path}", DebugMessageType.INFO)
+        sorted_osm_file = sort_osm_file(input_osm_file_path)
+        extract_osm_file(sorted_osm_file, input_config_file_path, output_dir)
+        return True
+    elif "Relation IDs out of order" in result.stderr:
+        Debug.log(
+            f"Cannot extracted osm file: {input_osm_file_path}, Way IDs out of order",
+            DebugMessageType.ERROR,
+        )
+        Debug.log(f"Sorting osm file: {input_osm_file_path}", DebugMessageType.INFO)
+        sorted_osm_file = sort_osm_file(input_osm_file_path)
+        extract_osm_file(sorted_osm_file, input_config_file_path, output_dir)
+        return True
     else:
-        if "Output directory is missing or not accessible" in result.stderr:
-            Debug.log(
-                f"Cannot extracted osm file: {input_osm_file_path}, Output directory is missing or not accessible",
-                DebugMessageType.ERROR,
-            )
-            Debug.log(f"Creating output directory: {output_dir}", DebugMessageType.INFO)
-            os.mkdir(output_dir)
-            extract_osm_file(input_osm_file_path, input_config_file_path, output_dir)
-            return True
-        elif "Way IDs out of order" in result.stderr:
-            Debug.log(
-                f"Cannot extracted osm file: {input_osm_file_path}, Way IDs out of order",
-                DebugMessageType.ERROR,
-            )
-            Debug.log(f"Sorting osm file: {input_osm_file_path}", DebugMessageType.INFO)
-            sorted_osm_file = sort_osm_file(input_osm_file_path)
-            extract_osm_file(sorted_osm_file, input_config_file_path, output_dir)
-            return True
-        elif "Relation IDs out of order" in result.stderr:
-            Debug.log(
-                f"Cannot extracted osm file: {input_osm_file_path}, Way IDs out of order",
-                DebugMessageType.ERROR,
-            )
-            Debug.log(f"Sorting osm file: {input_osm_file_path}", DebugMessageType.INFO)
-            sorted_osm_file = sort_osm_file(input_osm_file_path)
-            extract_osm_file(sorted_osm_file, input_config_file_path, output_dir)
-            return True
-        else:
-            Debug.log(
-                f"Cannot extracted osm file: {input_osm_file_path}, {result.stderr}",
-                DebugMessageType.ERROR,
-            )
-            print(result.stderr)
-            return False
+        Debug.log(
+            f"Cannot extracted osm file: {input_osm_file_path}, {result.stderr}",
+            DebugMessageType.ERROR,
+        )
+        print(result.stderr)
+        return False
 
 
 def sort_osm_file(input_osm_file_path: str) -> str:
