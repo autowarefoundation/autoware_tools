408334481af816639d748bd7741d3c67789b3047
TaikiYamada4 <taiki.yamada@tier4.jp>
2024-11-26 11:06:35 +0900

    Added valdiator missing_referrers_for_traffic_lights
    Signed-off-by: TaikiYamada4 <taiki.yamada@tier4.jp>

---
+++ map/autoware_lanelet2_map_validator/docs/traffic_light/missing_referrers_for_traffic_lights.md
@@ -0,0 +1,18 @@
+# missing_referrers_for_traffic_lights
+
+## Validator name
+
+mapping.traffic_light.missing_referrers
+
+## Feature
+
+This validator checks whether each `traffic_light` type regulatory element has been referred by at least one lanelet.
+
+| Issue Code                        | Message                                                                       | Severity | Primitive          | Description                                                                                  | Approach                                                                                   |
+| --------------------------------- | ----------------------------------------------------------------------------- | -------- | ------------------ | -------------------------------------------------------------------------------------------- | ------------------------------------------------------------------------------------------ |
+| TrafficLight.MissingReferrers-001 | Regulatory element of traffic light must be referred by at least one lanelet. | Error    | Regulatory Element | There is a `traffic_light` type regulatory element that hasn't been referred by any lanelet. | The lanelet that might be blocked by the traffic light must refer this regulatory element. |
+
+## Related source codes
+
+- missing_referrers_for_traffic_lights.cpp
+- missing_referrers_for_traffic_lights.hpp
---
+++ map/autoware_lanelet2_map_validator/src/validators/traffic_light/missing_referrers_for_traffic_lights.cpp
@@ -0,0 +1,83 @@
+// Copyright 2024 Autoware Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "lanelet2_map_validator/validators/traffic_light/missing_referrers_for_traffic_lights.hpp"
+
+#include "lanelet2_map_validator/utils.hpp"
+
+#include <lanelet2_core/LaneletMap.h>
+
+namespace lanelet::autoware::validation
+{
+namespace
+{
+lanelet::validation::RegisterMapValidator<MissingReferrersForTrafficLightsValidator> reg;
+}
+
+lanelet::validation::Issues MissingReferrersForTrafficLightsValidator::operator()(
+  const lanelet::LaneletMap & map)
+{
+  lanelet::validation::Issues issues;
+
+  lanelet::autoware::validation::appendIssues(
+    issues, check_missing_referrers_for_traffic_lights(map));
+
+  return issues;
+}
+
+lanelet::validation::Issues
+MissingReferrersForTrafficLightsValidator::check_missing_referrers_for_traffic_lights(
+  const lanelet::LaneletMap & map)
+{
+  lanelet::validation::Issues issues;
+
+  for (const lanelet::RegulatoryElementConstPtr & reg_elem : map.regulatoryElementLayer) {
+    // Skip non traffic light regulatory elements
+    if (
+      reg_elem->attribute(lanelet::AttributeName::Subtype).value() !=
+      lanelet::AttributeValueString::TrafficLight) {
+      continue;
+    }
+
+    // At least one lanelet should refer a traffic_light regulatory element
+    const lanelet::ConstLanelets referring_lanelets =
+      collect_referring_lanelets(map, reg_elem->id());
+    if (referring_lanelets.size() == 0) {
+      issues.emplace_back(
+        lanelet::validation::Severity::Error, lanelet::validation::Primitive::RegulatoryElement,
+        reg_elem->id(),
+        issue_code_prefix(this->name(), 1) +
+          "Regulatory element of traffic light must be referred by at least one lanelet.");
+      continue;
+    }
+  }
+
+  return issues;
+}
+
+lanelet::ConstLanelets MissingReferrersForTrafficLightsValidator::collect_referring_lanelets(
+  const lanelet::LaneletMap & map, const lanelet::Id target_reg_elem_id)
+{
+  lanelet::ConstLanelets lanelet_group;
+  for (const lanelet::ConstLanelet & lanelet : map.laneletLayer) {
+    for (lanelet::RegulatoryElementConstPtr reg_elem : lanelet.regulatoryElements()) {
+      if (reg_elem->id() == target_reg_elem_id) {
+        lanelet_group.push_back(lanelet);
+      }
+    }
+  }
+  return lanelet_group;
+}
+
+}  // namespace lanelet::autoware::validation
