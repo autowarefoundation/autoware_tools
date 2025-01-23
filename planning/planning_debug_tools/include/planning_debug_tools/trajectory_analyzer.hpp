aa738de2fc07cc9fde0f6a8756a120687982be28
Ryohsuke Mitsudome <43976834+mitsudome-r@users.noreply.github.com>
2025-01-23 12:45:57 +0900

    feat(planning_debug_tools)!: replace tier4_debug_msgs with tier4_internal_debug_msgs (#200)
    Signed-off-by: Ryohsuke Mitsudome <ryohsuke.mitsudome@tier4.jp>

--- planning/planning_debug_tools/include/planning_debug_tools/trajectory_analyzer.hpp
+++ planning/planning_debug_tools/include/planning_debug_tools/trajectory_analyzer.hpp
@@ -21,10 +21,10 @@
 #include "planning_debug_tools/util.hpp"
 #include "rclcpp/rclcpp.hpp"
 
+#include "autoware_internal_debug_msgs/msg/float64_multi_array_stamped.hpp"
 #include "autoware_planning_msgs/msg/path.hpp"
 #include "autoware_planning_msgs/msg/trajectory.hpp"
 #include "nav_msgs/msg/odometry.hpp"
-#include "tier4_debug_msgs/msg/float64_multi_array_stamped.hpp"
 #include "tier4_planning_msgs/msg/path_with_lane_id.hpp"
 
 #include <iostream>
