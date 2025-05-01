f131344df866b920716a99d740e5627716800814
Takayuki Murooka <takayuki5168@gmail.com>
2025-05-01 14:40:30 +0900

    feat(static_centerline_generator): visualize iterative planning results and introduce glog (#241)
    * add glog
    
    Signed-off-by: Takayuki Murooka <takayuki5168@gmail.com>
    
    * fix
    
    Signed-off-by: Takayuki Murooka <takayuki5168@gmail.com>
    
    * visualize iterative trajectory planning
    
    Signed-off-by: Takayuki Murooka <takayuki5168@gmail.com>
    
    * fix conflict
    
    Signed-off-by: Takayuki Murooka <takayuki5168@gmail.com>
    
    * fix
    
    Signed-off-by: Takayuki Murooka <takayuki5168@gmail.com>
    
    * fix cmake
    
    Signed-off-by: Takayuki Murooka <takayuki5168@gmail.com>
    
    * add glog in build_depends.repos
    
    Signed-off-by: Takayuki Murooka <takayuki5168@gmail.com>
    
    * fix build_depends.repos
    
    Signed-off-by: Takayuki Murooka <takayuki5168@gmail.com>
    
    ---------
    
    Signed-off-by: Takayuki Murooka <takayuki5168@gmail.com>

--- planning/autoware_static_centerline_generator/src/centerline_source/optimization_trajectory_based_centerline.hpp
+++ planning/autoware_static_centerline_generator/src/centerline_source/optimization_trajectory_based_centerline.hpp
@@ -32,10 +32,13 @@ public:
     const std::vector<lanelet::Id> & route_lane_ids);
 
 private:
-  std::vector<TrajectoryPoint> optimize_trajectory(const Path & raw_path) const;
+  std::vector<TrajectoryPoint> optimize_trajectory(
+    rclcpp::Node & node, const Path & raw_path) const;
 
   rclcpp::Publisher<PathWithLaneId>::SharedPtr pub_raw_path_with_lane_id_{nullptr};
   rclcpp::Publisher<Path>::SharedPtr pub_raw_path_{nullptr};
+  rclcpp::Publisher<Trajectory>::SharedPtr pub_iterative_smoothed_traj_{nullptr};
+  rclcpp::Publisher<Trajectory>::SharedPtr pub_iterative_optimized_traj_{nullptr};
 };
 }  // namespace autoware::static_centerline_generator
 // clang-format off
