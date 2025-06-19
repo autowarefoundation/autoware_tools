37998aa92567bac8faab4f349cb12ba68f819046
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2025-06-19 16:36:31 +0900

    Added acceleration evaluation
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- localization/autoware_localization_evaluation_scripts/scripts/analyze_rosbags_parallel.py
+++ localization/autoware_localization_evaluation_scripts/scripts/analyze_rosbags_parallel.py
@@ -6,19 +6,25 @@ import json
 from multiprocessing import Pool
 from pathlib import Path
 
+import pandas as pd
+
 import compare_trajectories
 import extract_values_from_rosbag
-import pandas as pd
 import plot_diagnostics
+from utils.calc_acceleration_diff import calc_acceleration_diff
 
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument("result_dir", type=Path)
     parser.add_argument("--parallel_num", type=int, default=1)
-    parser.add_argument("--topic_subject", type=str, default="/localization/kinematic_state")
     parser.add_argument(
-        "--topic_reference", type=str, default="/localization/pose_estimator/pose_with_covariance"
+        "--topic_subject", type=str, default="/localization/kinematic_state"
+    )
+    parser.add_argument(
+        "--topic_reference",
+        type=str,
+        default="/localization/pose_estimator/pose_with_covariance",
     )
     parser.add_argument("--save_dir_relative", type=str, default="")
     return parser.parse_args()
@@ -37,8 +43,12 @@ def process_directory(
     plot_diagnostics.main(rosbag_path=target_rosbag, save_dir=diagnostics_result_dir)
 
     # (2) Extract values from rosbag
-    save_name_subject = extract_values_from_rosbag.topic_name_to_save_name(topic_subject)
-    save_name_reference = extract_values_from_rosbag.topic_name_to_save_name(topic_reference)
+    save_name_subject = extract_values_from_rosbag.topic_name_to_save_name(
+        topic_subject
+    )
+    save_name_reference = extract_values_from_rosbag.topic_name_to_save_name(
+        topic_reference
+    )
     extract_values_from_rosbag.main(
         rosbag_path=target_rosbag,
         target_topics=[
@@ -78,14 +88,13 @@ def process_directory(
 
     # (6-1) relative pose
     relative_pose_tsv = (
-        save_dir / "compare_trajectories/localization__kinematic_state_result/relative_pose.tsv"
+        save_dir
+        / "compare_trajectories/localization__kinematic_state_result/relative_pose.tsv"
     )
     df = pd.read_csv(relative_pose_tsv, sep="\t")
-    mean_position_norm = df["position.norm"].mean()
-    mean_angle_norm = df["angle.norm"].mean()
-    mean_linear_velocity_norm = df["linear_velocity.norm"].mean()
-    mean_angular_velocity_norm = df["angular_velocity.norm"].mean()
 
+    # position
+    mean_position_norm = df["position.norm"].mean()
     THRESHOLD_MEAN_POSITION_NORM = 0.5
     if mean_position_norm > THRESHOLD_MEAN_POSITION_NORM:
         final_success = False
@@ -93,6 +102,8 @@ def process_directory(
     else:
         final_summary += f"{mean_position_norm=:.3f} [m]"
 
+    # angle
+    mean_angle_norm = df["angle.norm"].mean()
     THRESHOLD_MEAN_ANGLE_NORM = 0.5
     if mean_angle_norm > THRESHOLD_MEAN_ANGLE_NORM:
         final_success = False
@@ -100,19 +111,48 @@ def process_directory(
     else:
         final_summary += f"|{mean_angle_norm=:.3f} [deg]"
 
-    THRESHOLD_MEAN_LINEAR_VELOCITY_NORM = 0.05
-    if mean_linear_velocity_norm > THRESHOLD_MEAN_LINEAR_VELOCITY_NORM:
-        final_success = False
-        final_summary += f"|{mean_linear_velocity_norm=:.3f} [m/s] is too large."
-    else:
-        final_summary += f"|{mean_linear_velocity_norm=:.3f} [m/s]"
+    # linear velocity
+    if "linear_velocity.norm" in df.columns:
+        mean_linear_velocity_norm = df["linear_velocity.norm"].mean()
+        THRESHOLD_MEAN_LINEAR_VELOCITY_NORM = 0.05
+        if mean_linear_velocity_norm > THRESHOLD_MEAN_LINEAR_VELOCITY_NORM:
+            final_success = False
+            final_summary += f"|{mean_linear_velocity_norm=:.3f} [m/s] is too large."
+        else:
+            final_summary += f"|{mean_linear_velocity_norm=:.3f} [m/s]"
 
-    THRESHOLD_MEAN_ANGULAR_VELOCITY_NORM = 0.05
-    if mean_angular_velocity_norm > THRESHOLD_MEAN_ANGULAR_VELOCITY_NORM:
-        final_success = False
-        final_summary += f"|{mean_angular_velocity_norm=:.3f} [rad/s] is too large."
-    else:
-        final_summary += f"|{mean_angular_velocity_norm=:.3f} [rad/s]"
+    # angular velocity
+    if "angular_velocity.norm" in df.columns:
+        mean_angular_velocity_norm = df["angular_velocity.norm"].mean()
+        THRESHOLD_MEAN_ANGULAR_VELOCITY_NORM = 0.05
+        if mean_angular_velocity_norm > THRESHOLD_MEAN_ANGULAR_VELOCITY_NORM:
+            final_success = False
+            final_summary += f"|{mean_angular_velocity_norm=:.3f} [rad/s] is too large."
+        else:
+            final_summary += f"|{mean_angular_velocity_norm=:.3f} [rad/s]"
+
+    # acceleration
+    df_ref = pd.read_csv(compare_result_dir / f"{save_name_reference}.tsv", sep="\t")
+    if "linear_velocity.x" in df_ref.columns:
+        THRESHOLD_MEAN_ACCELERATION_NORM = 0.5
+        acceleration_tsv = (
+            save_dir / "result_acceleration/localization__acceleration.tsv"
+        )
+        df_acceleration = pd.read_csv(acceleration_tsv, sep="\t")
+        df_result = calc_acceleration_diff(df_acceleration, df_ref)
+        df_result.to_csv(
+            save_dir / "result_acceleration/localization__acceleration_diff.tsv",
+            sep="\t",
+            index=False,
+        )
+        mean_acceleration_norm_diff = df_result["diff.norm"].mean()
+
+        # Check threshold
+        if mean_acceleration_norm_diff > THRESHOLD_MEAN_ACCELERATION_NORM:
+            final_success = False
+            final_summary += f"|{mean_acceleration_norm_diff=:.3f} [m/s²] is too large."
+        else:
+            final_summary += f"|{mean_acceleration_norm_diff=:.3f} [m/s²]"
 
     # (6-2) diagnostics
     target_tsv_list = [
@@ -125,7 +165,10 @@ def process_directory(
         localization_ekf_diagnostics_tsv = diagnostics_result_dir / f"{target_tsv}.tsv"
         df = pd.read_csv(localization_ekf_diagnostics_tsv, sep="\t")
         # filter out WARNs before activation
-        df = df[df["message"] != "[WARN]process is not activated; [WARN]initial pose is not set"]
+        df = df[
+            df["message"]
+            != "[WARN]process is not activated; [WARN]initial pose is not set"
+        ]
         not_ok_num = len(df[df["level"] != 0])
         if len(df) == 0:
             not_ok_percentage = 0
@@ -159,7 +202,9 @@ if __name__ == "__main__":
     topic_reference = args.topic_reference
     save_dir_relative = args.save_dir_relative
 
-    target_rosbags = list(result_dir.glob("**/*.db3")) + list(result_dir.glob("**/*.mcap"))
+    target_rosbags = list(result_dir.glob("**/*.db3")) + list(
+        result_dir.glob("**/*.mcap")
+    )
     directories = [path.parent.parent for path in target_rosbags]
     directories = list(set(directories))
     directories = [d for d in directories if not d.is_symlink()]
@@ -168,5 +213,8 @@ if __name__ == "__main__":
     with Pool(args.parallel_num) as pool:
         pool.starmap(
             process_directory,
-            [(d, topic_subject, topic_reference, save_dir_relative) for d in directories],
+            [
+                (d, topic_subject, topic_reference, save_dir_relative)
+                for d in directories
+            ],
         )
