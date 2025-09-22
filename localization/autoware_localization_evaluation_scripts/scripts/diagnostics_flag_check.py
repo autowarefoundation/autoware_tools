7d3cc59a6ec7ce5a5923d21503fdfa4f14f67998
TaikiYamada4 <taiki.yamada@tier4.jp>
2025-09-22 11:31:17 +0900

    refactor script
    Signed-off-by: TaikiYamada4 <taiki.yamada@tier4.jp>

--- localization/autoware_localization_evaluation_scripts/scripts/diagnostics_flag_check.py
+++ localization/autoware_localization_evaluation_scripts/scripts/diagnostics_flag_check.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """A script to check rise/fall of diagnostics flags."""
 
+from abc import abstractmethod
 import argparse
 from dataclasses import dataclass
 from pathlib import Path
 from typing import Dict
+from typing import Optional
 
 import pandas as pd
 import yaml
@@ -12,11 +14,152 @@ import yaml
 
 @dataclass
 class CriteriaConfig:
-    tsv_path: Path
-    diagnostics_key: str
-    target_time_ns: int
+    tsv_path: Path = "default.tsv"
+    diagnostics_key: str = ""
     flag: str = "rise"
-    time_window_ns: int = 200_000_000
+    target_time_ns: Optional[int] = None
+    timestamp_label = "timestamp_header"
+    time_window_ns: int = 500_000_000
+
+
+class BaseChecker:
+    def __init__(self, cfg: CriteriaConfig):
+        self.cfg = cfg
+        if not self.cfg.tsv_path.exists():
+            print(f"TSV file not found for {self.cfg.diagnostics_key}: {self.cfg.tsv_path}")
+            return False
+        self.df = pd.read_csv(cfg.tsv_path, sep="\t")
+
+    @abstractmethod
+    def is_positive(self, series: pd.Series) -> bool:
+        pass
+
+    def any_positive_in_window(self) -> bool:
+        start = self.cfg.target_time_ns - self.cfg.time_window_ns
+        end = self.cfg.target_time_ns + self.cfg.time_window_ns
+
+        df_valid = self.df.dropna(subset=[self.cfg.timestamp_label])
+        df_valid = df_valid.sort_values(by=self.cfg.timestamp_label)
+        timestamp_ns = pd.to_numeric(df_valid[self.cfg.timestamp_label], errors="coerce")
+        df_window = df_valid[(timestamp_ns >= start) & (timestamp_ns <= end)]
+
+        for _, row in df_window.iterrows():
+            if self.is_positive(row):
+                return True
+
+        return False
+
+    def any_negative_in_window(self) -> bool:
+        start = self.cfg.target_time_ns - self.cfg.time_window_ns
+        end = self.cfg.target_time_ns + self.cfg.time_window_ns
+
+        df_valid = self.df.dropna(subset=[self.cfg.timestamp_label])
+        df_valid = df_valid.sort_values(by=self.cfg.timestamp_label)
+        timestamp_ns = pd.to_numeric(df_valid[self.cfg.timestamp_label], errors="coerce")
+        df_window = df_valid[(timestamp_ns >= start) & (timestamp_ns <= end)]
+
+        for _, row in df_window.iterrows():
+            if not self.is_positive(row):
+                return True
+
+        return False
+
+    def all_positive_before_window(self) -> bool:
+        start = self.cfg.target_time_ns - self.cfg.time_window_ns
+        df_valid = self.df.dropna(subset=[self.cfg.timestamp_label])
+        df_valid = df_valid.sort_values(by=self.cfg.timestamp_label)
+        timestamp_ns = pd.to_numeric(df_valid[self.cfg.timestamp_label], errors="coerce")
+        df_window = df_valid[(timestamp_ns >= 0) & (timestamp_ns <= start)]
+
+        for _, row in df_window.iterrows():
+            if not self.is_positive(row):
+                return False
+
+        return True
+
+    def all_negative_before_window(self) -> bool:
+        start = self.cfg.target_time_ns - self.cfg.time_window_ns
+        df_valid = self.df.dropna(subset=[self.cfg.timestamp_label])
+        df_valid = df_valid.sort_values(by=self.cfg.timestamp_label)
+        timestamp_ns = pd.to_numeric(df_valid[self.cfg.timestamp_label], errors="coerce")
+        df_window = df_valid[(timestamp_ns >= 0) & (timestamp_ns <= start)]
+
+        for _, row in df_window.iterrows():
+            if self.is_positive(row):
+                return False
+
+        return True
+
+    def check(self) -> bool:
+        if self.cfg.flag == "rise":
+            if not self.any_positive_in_window():
+                print("    Not positive around target time.")
+                return False
+            if not self.all_negative_before_window():
+                print("    Found positive before target time.")
+                return False
+            return True
+        else:
+            if not self.any_negative_in_window():
+                print("    Not negative around target time.")
+                return False
+            if not self.all_positive_before_window():
+                print("    Found negative before target time.")
+                return False
+            return True
+
+
+class ImuTimeStampDtChecker(BaseChecker):
+    def is_positive(self, series):
+        level_label = "level"
+        stamp_label = "imu_time_stamp_dt"
+        if level_label in series and stamp_label in series:
+            return series[level_label] == 2 and series[stamp_label] >= 0.2
+        else:
+            return False
+
+
+class VehicleTwistTimeStampDtChecker(BaseChecker):
+    def is_positive(self, series):
+        level_label = "level"
+        stamp_label = "vehicle_twist_time_stamp_dt"
+        if level_label in series and stamp_label in series:
+            return series[level_label] == 2 and series[stamp_label] >= 0.2
+        else:
+            return False
+
+
+class PoseNoUpdateCountChecker(BaseChecker):
+    def is_positive(self, series):
+        level_label = "level"
+        count_label = "pose_no_update_count"
+        threshold_label = "pose_no_update_count_threshold_error"
+        if level_label in series and count_label in series and threshold_label in series:
+            return series[level_label] == 2 and series[count_label] >= series[threshold_label]
+        else:
+            return False
+
+
+class PoseIsPassedDelayGateChecker(BaseChecker):
+    def is_positive(self, series):
+        level_label = "level"
+        is_passed_label = "pose_is_passed_delay_gate"
+        if level_label in series and is_passed_label in series:
+            return series[level_label] == 2 and not series[is_passed_label]
+        else:
+            return False
+
+
+class IsInitialPoseReliableChecker(BaseChecker):
+    # Override check() since this diagnostics is special
+    def check(self):
+        is_initial_pose_reliable = self.df.iloc[-1]["is_initial_pose_reliable"]
+        if (self.cfg.flag == "rise" and not is_initial_pose_reliable) or (
+            self.cfg.flag == "fall" and is_initial_pose_reliable
+        ):
+            return True
+        else:
+            return False
 
 
 def parse_args() -> argparse.Namespace:
@@ -37,151 +180,6 @@ def load_diagnostics_flag_check(yaml_path: Path):
     return conditions.get("DiagnosticsFlagCheck", None)
 
 
-def check_pose_no_update_count(cfg: CriteriaConfig) -> bool:
-    if not cfg.tsv_path.exists():
-        print(f"TSV file not found for {cfg.diagnostics_key}: {cfg.tsv_path}")
-        return False
-    df = pd.read_csv(cfg.tsv_path, sep="\t")
-
-    required_cols = [
-        "pose_no_update_count",
-        "pose_no_update_count_threshold_error",
-        "timestamp_header",
-        "message",
-    ]
-    error_message = "[ERROR]pose is not updated"
-
-    for col in required_cols:
-        if col not in df.columns:
-            print(f"Missing required column: {col}")
-            return False
-
-    # Convert timestamps into integer nanoseconds
-    df["timestamp_ns"] = pd.to_numeric(df["timestamp_header"], errors="coerce")
-
-    # Define ranges
-    start = cfg.target_time_ns - cfg.time_window_ns
-    end = cfg.target_time_ns + cfg.time_window_ns
-
-    df_numeric = df.dropna(subset=required_cols)
-
-    # Split into pre-window, in-window
-    df_pre = df_numeric[df_numeric["timestamp_ns"] < start]
-    df_window = df_numeric[
-        (df_numeric["timestamp_ns"] >= start) & (df_numeric["timestamp_ns"] <= end)
-    ]
-
-    # Define conditions
-    violation = (
-        df_numeric["pose_no_update_count"] >= df_numeric["pose_no_update_count_threshold_error"]
-    ) & (df_numeric["message"].str.contains(error_message, na=False, regex=False))
-    ok = ~violation
-
-    if cfg.flag == "rise":
-        # Expect violation inside the window, but *no violation* before it
-        if violation[df_pre.index].any():
-            print(f"{cfg.diagnostics_key}: Unexpected violation before window.")
-            return False
-        df_condition = df_window[violation[df_window.index]]
-    elif cfg.flag == "fall":
-        # Expect OK inside the window, but *only violation* before it
-        if ok[df_pre.index].any():
-            print(f"{cfg.diagnostics_key}: Unexpected OK state before window.")
-            return False
-        df_condition = df_window[ok[df_window.index]]
-    else:
-        print("Invalid flag is set")
-        return False
-
-    if df_condition.empty:
-        print(f"{cfg.diagnostics_key}: No matching rows found in window.")
-        return False
-
-    print(f"{cfg.diagnostics_key}: Matching rows from {cfg.tsv_path}:")
-    print(df_condition["pose_no_update_count"].iloc[0])
-    return True
-
-
-def check_pose_is_passed_delay_gate(cfg: CriteriaConfig) -> bool:
-    if not cfg.tsv_path.exists():
-        print(f"TSV file not found for {cfg.diagnostics_key}: {cfg.tsv_path}")
-        return False
-    df = pd.read_csv(cfg.tsv_path, sep="\t")
-
-    # Ensure columns exist
-    required_cols = [
-        "pose_is_passed_delay_gate",
-        "timestamp_header",
-        "message",
-    ]
-
-    error_message = "[WARN]pose topic is delay"
-
-    for col in required_cols:
-        if col not in df.columns:
-            print(f"Missing required column: {col}")
-            return False
-
-    # Convert timestamps into integer nanoseconds
-    df["pose_is_passed_delay_gate"] = df["pose_is_passed_delay_gate"].astype(bool)
-    df["timestamp_ns"] = pd.to_numeric(df["timestamp_header"], errors="coerce")
-
-    # Convert target into window range
-    start = cfg.target_time_ns - cfg.time_window_ns
-    end = cfg.target_time_ns + cfg.time_window_ns
-
-    df_pre = df[df["timestamp_ns"] < start]
-    df_window = df[(df["timestamp_ns"] >= start) & (df["timestamp_ns"] <= end)]
-
-    if cfg.flag == "rise":
-        # Expect False + no False before window
-        if (~df_pre["pose_is_passed_delay_gate"]).any():
-            print(f"{cfg.diagnostics_key}: Unexpected False before window.")
-            return False
-        df_condition = df_window[
-            (~df_window["pose_is_passed_delay_gate"])
-            & (df_window["message"].str.contains(error_message, na=False, regex=False))
-        ]
-    elif cfg.flag == "fall":
-        # Expect True + no True before window
-        if (df_pre["pose_is_passed_delay_gate"]).any():
-            print(f"{cfg.diagnostics_key}: Unexpected True before window.")
-            return False
-        df_condition = df_window[
-            (df_window["pose_is_passed_delay_gate"])
-            & (~df_window["message"].str.contains(error_message, na=True, regex=False))
-        ]
-    else:
-        print("Invalid flag is set")
-        return False
-
-    if df_condition.empty:
-        print(f"{cfg.diagnostics_key}: No matching rows found.")
-        return False
-
-    print(f"{cfg.diagnostics_key}: Matching rows from {cfg.tsv_path} at:")
-    print(df_condition["timestamp_header"].iloc[0])
-    return True
-
-
-def check_is_initial_pose_reliable(cfg: CriteriaConfig) -> bool:
-    if not cfg.tsv_path.exists():
-        print(f"TSV file not found for {cfg.diagnostics_key}: {cfg.tsv_path}")
-        return False
-    df = pd.read_csv(cfg.tsv_path, sep="\t", dtype={"is_initial_pose_reliable": "boolean"})
-
-    # We will just see the final row for pose_initializer diagnostics. In other words, the timing is not considered
-
-    is_initial_pose_reliable = df.iloc[-1]["is_initial_pose_reliable"]  # get the last result
-    if (cfg.flag == "rise" and is_initial_pose_reliable) or (
-        cfg.flag == "fall" and not is_initial_pose_reliable
-    ):
-        return True
-
-    print("pose_initializer didn't work as expected.")
-    return False
-
-
 def main(scenario_file: Path, diagnostics_result_dir: Path) -> Dict[str, bool]:
     diagnostics_flag_condition = load_diagnostics_flag_check(scenario_file)
 
@@ -191,33 +189,48 @@ def main(scenario_file: Path, diagnostics_result_dir: Path) -> Dict[str, bool]:
 
     results = {key: False for key in diagnostics_flag_condition}
     for key, criteria in diagnostics_flag_condition.items():
-        print(f"\nProcessing criteria: {key}")
+        print(f"Processing diagnostics: {key}")
 
-        flag = criteria.get("flag", "").strip().lower()
+        criteria_cfg = CriteriaConfig()
+
+        criteria_cfg.key = key
+        criteria_cfg.flag = criteria.get("flag", "").strip().lower()
+
         at_sec = int(float(criteria.get("at_sec", 0)))
         at_nanosec = int(float(criteria.get("at_nanosec", 0)))
-        target_time = int(at_sec * 1_000_000_000 + at_nanosec)
+        criteria_cfg.target_time_ns = int(at_sec * 1_000_000_000 + at_nanosec)
 
-        if key == "pose_no_update_count":
-            tsv_path = diagnostics_result_dir / "localization__ekf_localizer.tsv"
-            criteria_cfg = CriteriaConfig(
-                tsv_path=tsv_path, diagnostics_key=key, flag=flag, target_time_ns=target_time
-            )
-            results[key] = check_pose_no_update_count(criteria_cfg)
+        if not (criteria_cfg.flag == "rise" or criteria_cfg.flag == "fall"):
+            print("    Invalid flag found!")
+            results[key] = False
+        elif key == "pose_no_update_count":
+            criteria_cfg.tsv_path = diagnostics_result_dir / "localization__ekf_localizer.tsv"
+            results[key] = PoseNoUpdateCountChecker(criteria_cfg).check()
+            # results[key] = check_pose_no_update_count(criteria_cfg)
         elif key == "pose_is_passed_delay_gate":
-            tsv_path = diagnostics_result_dir / "localization__ekf_localizer.tsv"
-            criteria_cfg = CriteriaConfig(
-                tsv_path=tsv_path, diagnostics_key=key, flag=flag, target_time_ns=target_time
-            )
-            results[key] = check_pose_is_passed_delay_gate(criteria_cfg)
+            criteria_cfg.tsv_path = diagnostics_result_dir / "localization__ekf_localizer.tsv"
+            results[key] = PoseIsPassedDelayGateChecker(criteria_cfg).check()
+            # results[key] = check_pose_is_passed_delay_gate(criteria_cfg)
         elif key == "is_initial_pose_reliable":
-            tsv_path = diagnostics_result_dir / "pose_initializer__pose_initializer_status.tsv"
-            criteria_cfg = CriteriaConfig(
-                tsv_path=tsv_path, diagnostics_key=key, flag=flag, target_time_ns=target_time
+            criteria_cfg.tsv_path = (
+                diagnostics_result_dir / "pose_initializer__pose_initializer_status.tsv"
             )
-            results[key] = check_is_initial_pose_reliable(criteria_cfg)
+            results[key] = IsInitialPoseReliableChecker(criteria_cfg).check()
+            # results[key] = check_is_initial_pose_reliable(criteria_cfg)
+        elif key == "imu_time_stamp_dt":
+            criteria_cfg.tsv_path = (
+                diagnostics_result_dir / "gyro_odometer__gyro_odometer_status.tsv"
+            )
+            results[key] = ImuTimeStampDtChecker(criteria_cfg).check()
+        elif key == "vehicle_twist_time_stamp_dt":
+            criteria_cfg.tsv_path = (
+                diagnostics_result_dir / "gyro_odometer__gyro_odometer_status.tsv"
+            )
+            results[key] = VehicleTwistTimeStampDtChecker(criteria_cfg).check()
         else:
             print(f"Flag checking for {key} not defined!!")
+
+        print(f"    {results[key]}")
     return results
 
 
