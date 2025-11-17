46bafb97f1ae80439ddb54716c82f46b8949c48f
Lewis Smith <lewisjohnsmith2@gmail.com>
2025-11-17 15:52:35 +0900

    Working listener added for initial pose, checkpoint and goal poses.
    Signed-off-by: Lewis Smith <lewisjohnsmith2@gmail.com>

---
+++ planning/pose_replay/src/topic_listener.cpp
@@ -0,0 +1,41 @@
+#include <memory>
+#include <functional>
+
+#include "rclcpp/rclcpp.hpp" 
+#include "geometry_msgs/msg/pose_with_covariance_stamped.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+
+class TopicListener : public rclcpp::Node {
+
+	public:
+	TopicListener() : Node("topic_listener") {
+		RCLCPP_INFO_STREAM(this->get_logger(), "Listener online."); 
+
+		// /initialpose for initial pose
+		initial_pose_subscription_ = this->create_subscription<geometry_msgs::msg::PoseWithCovarianceStamped>(
+			"initialpose", 10, std::bind(&TopicListener::initial_pose_topic_callback, this, std::placeholders::_1));
+
+		// /planning/mission_planning/goal for goal - currently this includes checkpoints, need to distinguish
+		goal_pose_subscription_ = this->create_subscription<geometry_msgs::msg::PoseStamped>(
+			"planning/mission_planning/goal", 10, std::bind(&TopicListener::goal_pose_topic_callback, this, std::placeholders::_1));
+	};	
+
+	private:
+	void initial_pose_topic_callback(const geometry_msgs::msg::PoseWithCovarianceStamped& msg) const 
+	{
+		RCLCPP_INFO(this->get_logger(), "I heard: '%f'", msg.pose.pose.position.x);
+	}
+	void goal_pose_topic_callback(const geometry_msgs::msg::PoseStamped& msg) const 
+	{
+		RCLCPP_INFO(this->get_logger(), "I heard: '%f'", msg.pose.position.x);
+	}
+	rclcpp::Subscription<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr initial_pose_subscription_;
+	rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr goal_pose_subscription_;
+
+};
+
+int main(int argc, char * argv[]){
+	rclcpp::init(argc, argv);
+	rclcpp::spin(std::make_shared<TopicListener>());
+	rclcpp::shutdown();
+}
