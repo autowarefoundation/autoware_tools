98638f4e88aebf069b469260370707c0def02050
Lewis Smith <lewisjohnsmith2@gmail.com>
2025-11-25 19:20:01 +0900

    json replaced for yaml writing
    Signed-off-by: Lewis Smith <lewisjohnsmith2@gmail.com>

--- planning/pose_replay/src/topic_listener.cpp
+++ planning/pose_replay/src/topic_listener.cpp
@@ -16,7 +16,9 @@
 #include "autoware_adapi_v1_msgs/srv/clear_route.hpp"
 #include <chrono>
 
+
 using namespace std::literals::chrono_literals;
+using autoware_adapi_v1_msgs::msg::to_yaml;
 
 class TopicListener : public rclcpp::Node {
 
@@ -25,54 +27,26 @@ class TopicListener : public rclcpp::Node {
 		RCLCPP_INFO_STREAM(this->get_logger(), "Listener online."); 
 		
 		route_set_subscription_ = this->create_subscription<autoware_adapi_v1_msgs::msg::Route>(
-			"/api/routing/route", 10, std::bind(&TopicListener::test_callback, this, std::placeholders::_1));
+			"/api/routing/route", 10, std::bind(&TopicListener::route_set_callback, this, std::placeholders::_1));
 		
 	//	client_ = this->create_client<autoware_adapi_v1_msgs::srv::ClearRoute>("/api/routing/clear_route");
 	};
 
-	void test_callback(const autoware_adapi_v1_msgs::msg::Route& msg)
+	void route_set_callback(const autoware_adapi_v1_msgs::msg::Route& msg)
 	{	
 		if(msg.data.empty()) return;
-		
-		auto data = msg.data[0];
-		
-		auto start = data.start;
-		auto goal = data.goal;
-
-		nlohmann::json ji = pose_to_json(start);
-		nlohmann::json jg = pose_to_json(goal);	
-		
-		fwrite_json(ji);
-		fwrite_json(jg);
-	
-		RCLCPP_INFO(this->get_logger(), "Route log start: %f", msg.data[0].start.position.x);
-		RCLCPP_INFO(this->get_logger(), "Route log end: %f", msg.data[0].goal.position.x);
-	}
-	
-	nlohmann::json pose_to_json(geometry_msgs::msg::Pose p) 
-	{	
-		nlohmann::json j;
-		
-		j["uuid"] = 1;
-		j["type"] = "initial / goal";
-		j["pose"]["position"]["x"] = p.position.x;
-		j["pose"]["position"]["y"] = p.position.y;
-		j["pose"]["position"]["z"] = p.position.z;
-		j["pose"]["orientation"]["x"] = p.orientation.x;
-		j["pose"]["orientation"]["y"] = p.orientation.y;
-		j["pose"]["orientation"]["z"] = p.orientation.z;
-		j["pose"]["orientation"]["w"] = p.orientation.w;
-
-		return j;
+		route_to_yaml(msg);
+		RCLCPP_INFO(this->get_logger(), "Route written to output.yaml.");
 	}
 
-	void fwrite_json(nlohmann::json j) 
-	{
+	void route_to_yaml(const autoware_adapi_v1_msgs::msg::Route& msg){
+		std::string yaml = autoware_adapi_v1_msgs::msg::to_yaml(msg);
 		std::ofstream o;
-		o.open("output.json", std::ios::app);
-		if(o.is_open()) o << j.dump(4);	
+		o.open("output.yaml", std::ios::app);
+		if(o.is_open()) o << yaml;	
 		o.close();
-	};
+	}	
+	
 
 	rclcpp::Subscription<autoware_adapi_v1_msgs::msg::Route>::SharedPtr route_set_subscription_;	
 //	rclcpp::Client<autoware_adapi_v1_msgs::srv::ClearRoute>::SharedPtr client_;
