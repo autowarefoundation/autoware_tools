22a150ddabb287d7440e3eeff110e3d87df2bc70
Lewis Smith <lewisjohnsmith2@gmail.com>
2026-01-21 14:22:56 +0900

    interfaces and node removal following refactor
    Signed-off-by: Lewis Smith <lewisjohnsmith2@gmail.com>

--- planning/pose_replay/pose_replay_panel/include/pose_replay_panel/pose_replay_panel.hpp
+++ planning/pose_replay/pose_replay_panel/include/pose_replay_panel/pose_replay_panel.hpp
@@ -1,13 +1,7 @@
 #ifndef POSE_REPLAY_PANEL__POSE_REPLAY_PANEL_HPP_
 #define POSE_REPLAY_PANEL__POSE_REPLAY_PANEL_HPP_
 
-#include "pose_replay_interfaces/srv/delete_route.hpp"
-#include "pose_replay_interfaces/srv/get_uuid_route.hpp"
-#include "pose_replay_interfaces/srv/get_uuid_routes.hpp"
-#include "pose_replay_interfaces/srv/set_name.hpp"
-#include "pose_replay_interfaces/srv/set_route.hpp"
-
-#include "pose_replay_panel/pose_replay_logic.hpp"
+#include "pose_replay_panel/pose_replay_node_abstract.hpp"
 
 #include <QLabel>
 #include <QPushButton>
@@ -17,6 +11,8 @@
 #include <rviz_common/ros_integration/ros_node_abstraction_iface.hpp>
 
 #include "std_msgs/msg/string.hpp"
+
+#include <qpushbutton.h>
 
 #include <string>
 
@@ -32,29 +28,16 @@ public:
 
 protected:
   std::shared_ptr<rviz_common::ros_integration::RosNodeAbstractionIface> node_ptr_;
-
-  // new
   rclcpp::Node::SharedPtr pose_replay_node_ptr_;
-  //
-
-  // rclcpp::Client<pose_replay_interfaces::srv::GetUuidRoutes>::SharedPtr get_routes_client_;
-  // rclcpp::Client<pose_replay_interfaces::srv::SetRoute>::SharedPtr set_route_client_;
-  // rclcpp::Client<pose_replay_interfaces::srv::DeleteRoute>::SharedPtr delete_route_client_;
-  // rclcpp::Client<pose_replay_interfaces::srv::SetName>::SharedPtr set_name_client_;
-  // rclcpp::Subscription<std_msgs::msg::String>::SharedPtr sync_notif_subscriber_;
-
+  std::unique_ptr<pose_replay::PoseReplayNode> node_abstract_;
   QVBoxLayout * dynamic_layout_;
 
-  std::unique_ptr<PoseReplay::PoseReplayLogic> logic_;
-
 private Q_SLOTS:
-  void button_activated();
   void route_entry_factory(const std::string &, const std::string &);
   void set_route_button_activated(const std::string &);
   void delete_route_button_activated(const std::string &);
   void sync_read();
   void clear_layout(QLayout *);
-  // void sync_notif_callback(std_msgs::msg::String);
   void rename_route_button_activated(const std::string &, const std::string &);
 };
 }  // namespace pose_replay_panel
--- planning/pose_replay/pose_replay_panel/src/pose_replay_panel.cpp
+++ planning/pose_replay/pose_replay_panel/src/pose_replay_panel.cpp
@@ -1,8 +1,4 @@
-#include "pose_replay_interfaces/srv/delete_route.hpp"
-#include "pose_replay_interfaces/srv/get_uuid_routes.hpp"
-#include "pose_replay_interfaces/srv/set_name.hpp"
-#include "pose_replay_interfaces/srv/set_route.hpp"
-#include "pose_replay_panel/pose_replay_logic.hpp"
+#include "pose_replay_panel/pose_replay_node_abstract.hpp"
 
 #include <QInputDialog>
 #include <QMessageBox>
@@ -22,16 +18,21 @@ using namespace std::literals::chrono_literals;
 
 namespace pose_replay_panel
 {
-using std::string;
 
 PoseReplayPanel::PoseReplayPanel(QWidget * parent) : Panel(parent)
 {
   const auto main_layout = new QVBoxLayout(this);
   main_layout->setAlignment(Qt::AlignTop);
   const auto main_title = new QLabel("Route history");
+  const auto save_btn = new QPushButton("Save current route");
+  QObject::connect(save_btn, &QPushButton::released, this, [this]() {
+    node_abstract_->save_route();
+    sync_read();
+  });
   dynamic_layout_ = new QVBoxLayout(this);
   dynamic_layout_->setAlignment(Qt::AlignTop);
   main_layout->addWidget(main_title);
+  main_layout->addWidget(save_btn);
   main_layout->addLayout(dynamic_layout_);
 }
 
@@ -41,50 +42,35 @@ void PoseReplayPanel::onInitialize()
 {
   node_ptr_ = getDisplayContext()->getRosNodeAbstraction().lock();
   rclcpp::Node::SharedPtr node = node_ptr_->get_raw_node();
-
-  //// new
   pose_replay_node_ptr_ = getDisplayContext()->getRosNodeAbstraction().lock()->get_raw_node();
-  logic_ = std::make_unique<PoseReplay::PoseReplayLogic>(pose_replay_node_ptr_);
-  ////
-
-  // get_routes_client_ = node->create_client<pose_replay_interfaces::srv::GetUuidRoutes>(
-  //   "/pose_replay/get_routes_service");
-  // set_route_client_ =
-  //   node->create_client<pose_replay_interfaces::srv::SetRoute>("/pose_replay/set_route_service");
-  // delete_route_client_ = node->create_client<pose_replay_interfaces::srv::DeleteRoute>(
-  //   "/pose_replay/delete_route_service");
-  // set_name_client_ =
-  //   node->create_client<pose_replay_interfaces::srv::SetName>("/pose_replay/set_name_service");
-  // sync_notif_subscriber_ = node->create_subscription<std_msgs::msg::String>(
-  //   "/pose_replay/update", 10,
-  //   std::bind(&PoseReplayPanel::sync_notif_callback, this, std::placeholders::_1));
-
+  node_abstract_ = std::make_unique<pose_replay::PoseReplayNode>(pose_replay_node_ptr_);
   sync_read();
 }
 
 void PoseReplayPanel::sync_read()
 {
   clear_layout(dynamic_layout_);
-  std::vector<PoseReplay::UuidName> fetched_routes = logic_->get_routes({});
+  std::vector<pose_replay::UuidName> fetched_routes = node_abstract_->get_routes({});
   for (auto & r : fetched_routes) {
     PoseReplayPanel::route_entry_factory(r.uuid, r.name);
   }
 }
 
-void PoseReplayPanel::route_entry_factory(const string & uuid, const std::string & name)
+void PoseReplayPanel::route_entry_factory(const std::string & uuid, const std::string & name)
 {
   auto layout = new QHBoxLayout();
   auto label = new QLabel(QString::fromStdString(name));
   auto load_btn = new QPushButton("Load");
   QObject::connect(
-    load_btn, &QPushButton::released, this, [this, uuid]() { set_route_button_activated(uuid); });
+    load_btn, &QPushButton::released, this, [this, uuid]() { node_abstract_->load_route(uuid); });
   auto rename_btn = new QPushButton("Rename");
   QObject::connect(rename_btn, &QPushButton::released, this, [this, name, uuid]() {
     rename_route_button_activated(name, uuid);
   });
   auto delete_btn = new QPushButton("Delete");
   QObject::connect(delete_btn, &QPushButton::released, this, [this, uuid]() {
-    delete_route_button_activated(uuid);
+    node_abstract_->delete_route(uuid);
+    sync_read();
   });
 
   layout->addWidget(label);
@@ -99,31 +85,17 @@ void PoseReplayPanel::rename_route_button_activated(
 {
   bool ok;
   QString currentName = QString::fromStdString(name);
-
   QString newName = QInputDialog::getText(
     this, tr("Rename"), tr("Enter new route name:"), QLineEdit::Normal, currentName, &ok);
-
   if (ok && !newName.isEmpty()) {
-    logic_->set_name(uuid, newName.toStdString());
+    node_abstract_->set_name(uuid, newName.toStdString());
     sync_read();
   }
-}
-
-void PoseReplayPanel::delete_route_button_activated(const std::string & uuid)
-{
-  logic_->delete_route(uuid);
-  sync_read();
-}
-
-void PoseReplayPanel::set_route_button_activated(const std::string & uuid)
-{
-  logic_->load_route(uuid);
 }
 
 void PoseReplayPanel::clear_layout(QLayout * layout)
 {
   if (!layout) return;
-
   QLayoutItem * item;
   while ((item = layout->takeAt(0)) != nullptr) {
     if (QWidget * widget = item->widget()) {
