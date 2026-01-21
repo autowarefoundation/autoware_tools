e4fb3bd30cf1a1159eceab2208a0cf2e84d063c0
Lewis Smith <lewisjohnsmith2@gmail.com>
2026-01-21 16:44:40 +0900

    save file loader added
    Signed-off-by: Lewis Smith <lewisjohnsmith2@gmail.com>

--- planning/pose_replay_panel/src/pose_replay_panel.cpp
+++ planning/pose_replay_panel/src/pose_replay_panel.cpp
@@ -1,14 +1,15 @@
 #include "pose_replay_panel/pose_replay_node_abstract.hpp"
 
+#include <QHBoxLayout>
 #include <QInputDialog>
 #include <QMessageBox>
 #include <QString>
 #include <QVBoxLayout>
-#include <QHBoxLayout>
 #include <pose_replay_panel/pose_replay_panel.hpp>
 #include <rviz_common/display_context.hpp>
 
 #include "std_msgs/msg/string.hpp"
+
 #include <qboxlayout.h>
 
 #include <chrono>
@@ -17,7 +18,7 @@
 #include <string>
 
 using namespace std::literals::chrono_literals;
- 
+
 namespace pose_replay_panel
 {
 
@@ -29,13 +30,18 @@ PoseReplayPanel::PoseReplayPanel(QWidget * parent) : Panel(parent)
   const auto title_layout = new QHBoxLayout(this);
   const auto main_title = new QLabel("Route history");
   const auto save_btn = new QPushButton("Save current route");
-  title_layout->addWidget(main_title);
-  title_layout->addWidget(save_btn);
+  const auto load_save_btn = new QPushButton("Load save file");
 
   QObject::connect(save_btn, &QPushButton::released, this, [this]() {
     node_abstract_->save_route();
     sync_read();
   });
+  QObject::connect(
+    load_save_btn, &QPushButton::released, this, [this]() { load_save_file_button_activated(); });
+
+  title_layout->addWidget(main_title);
+  title_layout->addWidget(load_save_btn);
+  title_layout->addWidget(save_btn);
 
   dynamic_layout_ = new QVBoxLayout(this);
   dynamic_layout_->setAlignment(Qt::AlignTop);
@@ -85,6 +91,18 @@ void PoseReplayPanel::route_entry_factory(const std::string & uuid, const std::s
   layout->addWidget(rename_btn);
   layout->addWidget(delete_btn);
   dynamic_layout_->addLayout(layout);
+}
+
+void PoseReplayPanel::load_save_file_button_activated()
+{
+  bool ok;
+  std::string edit_title = "Current save file: " + node_abstract_->get_save_path() + "\n" + "Enter save file path:";
+  QString new_path = QInputDialog::getText(
+    this, tr("Load save"), tr(edit_title.c_str()), QLineEdit::Normal, "", &ok);
+  if (ok && !new_path.isEmpty()) {
+    node_abstract_->set_save_path(new_path.toStdString());
+    sync_read();
+  }
 }
 
 void PoseReplayPanel::rename_route_button_activated(
